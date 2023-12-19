da611c1cfd8621ee27bca36a63cffe65852344eb
programmer-alt <modstils88@mail.ru>
2023-12-19 14:07:51 +0400

    in MyPosts.jsx used props.posts.map

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import s from './MyPosts.module.css';
 import Post from "../Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
  
 
- let postsElements = posts.map(post => <Post message={post.message} likesCount={post.likesCount} id={post.id}/>)
+ let postsElements = props.posts.map(post => <Post message={post.message} likesCount={post.likesCount} id={post.id}/>)
   return (
     <div>
       <div className={s.postsBlock}> <h3>My posts</h3> </div>
