f3fb3f519fe3c0c7f413f1153664a29d652cc960
programmer-alt <modstils88@mail.ru>
2023-12-25 21:00:01 +0400

    added div привет

--- src/components/Dialogs/DialogsItem/DialogItem.jsx
+++ src/components/Dialogs/DialogsItem/DialogItem.jsx
@@ -3,14 +3,15 @@ import s from './../Dialogs.module.css'
 import { NavLink } from "react-router-dom";
 
 const DialogItem = (props) => {
-    const imgSrc = "https://cdn-icons-png.flaticon.com/128/7005/7005152.png";
-    const imgSrcTwo = "https://img.freepik.com/free-vector/hand-drawn-black-music-logo-template_23-2150968998.jpg?size=626&ext=jpg&ga=GA1.1.369286752.1703489576&semt=sph"
+    const [imgSrc, imgSrcTwo] = ["https://cdn-icons-png.flaticon.com/128/7005/7005152.png", "https://img.freepik.com/free-vector/hand-drawn-black-music-logo-template_23-2150968998.jpg?size=626&ext=jpg&ga=GA1.1.369286752.1703489576&semt=sph"];
+
     let path = "/dialogs/" + props.id;
     let avatar = props.id > 3 ? imgSrc : imgSrcTwo
     return (
         <div className={s.dialog + ' ' + s.active}>
             <img src={avatar} alt="Avatar" className={s.avatar} />
             <NavLink to={path}> {props.name}</NavLink>
+            <div>привет</div>
         </div>
     )
 }
